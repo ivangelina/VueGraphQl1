558c8882bb7f2e3743cedb94cbf64402ca2a5559
fmi <fmi@fmi.uni-plovdiv.bg>
2019-04-14 10:01:40 +0300

    added mutations

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -4,4 +4,19 @@ export default {
   [mutations.SET_LOADER](state, isLoading) {
     state.isLoading = isLoading;
   },
+  [mutations.SET_USERS_LIST](state, usersList) {
+    state.usersList = usersList;
+  },
+  [mutations.SET_SELECTED_USER](state, user){
+    state.selectedUser = user;
+  },
+  [mutations.ADD_USER](state, user){
+    state.usersList.push(user);
+  },
+  [mutations.UPDATE_USER](state, {index, user}){
+    state.usersList.splice(index, 1, user);
+  },
+  [mutations.DELETE_USER](state, index){
+    state.usersList.splice(index, 1);
+  }
 };
