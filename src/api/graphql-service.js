252a322e9ea82c683be6830843521482873c8b61
fmi <fmi@fmi.uni-plovdiv.bg>
2019-04-14 10:19:10 +0300

    get users list action added

--- src/api/graphql-service.js
+++ src/api/graphql-service.js
@@ -5,4 +5,22 @@
 import gql from 'graphql-tag';
 import graphqlClient from './graphql';
 
-//TODO
+export default {
+
+    async getUsersList(params, success, failure) {
+        try {
+            const response = await graphqlClient.query({
+                query: gql`
+                    query {
+                        users {
+                            ${params}
+                        }
+                    }
+                `
+            });
+            success(response);
+        } catch (error){
+            failure(error);    
+        }
+    }
+}
--- src/store/actions.js
+++ src/store/actions.js
@@ -4,9 +4,20 @@ import dataService from '../api/data-service';
 import graphQlService from '../api/graphql-service';
 import getters from './getters';
 
-//TODO
 export default {
 
+    [actions.GET_USERS_LIST]({commit}, params= "_id username email") {
+        commit(mutations.SET_LOADER, true);
+        graphQlService.getUsersList(
+            params,
+            (response) => {
+                console.log(response);
+            },
+            (error) => {
+                console.log(error);
+            }
+        )
+    }
 
 
 };
