5c57aa1b7015ff1dcfe193fe79167fbd60ea6f43
unknown <ivangelina@abv.bg>
2019-05-01 17:50:40 +0300

    ObjectCommit

--- src/components/object/Object.vue
+++ src/components/object/Object.vue
@@ -65,29 +65,10 @@
     </div>
     <br>
     <div class="row">
-       <div class="col-md-12"> 
+     <div class="col-md-12"> 
      <div>
-  <b-carousel
-    id="carousel-fade"
-    style="text-shadow: 0px 0px 2px #000"
-    fade
-    indicators
-    img-width="1024"
-    img-height="480"
-  >
-    <b-carousel-slide
-      caption="First slide"
-      img-src="https://picsum.photos/1024/480/?image=87"
-    ></b-carousel-slide>
-    <b-carousel-slide
-      caption="Second Slide"
-      img-src="https://picsum.photos/1024/480/?image=27"
-    ></b-carousel-slide>
-    <b-carousel-slide
-      caption="Third Slide"
-      img-src="https://picsum.photos/1024/480/?image=10"
-    ></b-carousel-slide>
-  </b-carousel>
+        
+
 </div>
        </div>
     </div>
